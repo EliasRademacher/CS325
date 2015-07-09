847832b19e90cbfde2356dac4a3205404bfba571
EliasRademacher <rademace@onid.oregonstate.edu>
2015-07-08 17:43:00 -0700

    Extract method testArrayFunctions()

--- project1_tests.py
+++ project1_tests.py
@@ -1,25 +1,31 @@
 import maxSubArray
 
 
+	
+def testArrayFunctions(array, algorithms):
+	results = []
+	i = 0
+	for algorithm in algorithms:
+		results.append(algorithm(array))
+		print "Algorithm ", i + 1
+		print "Maximum Sum: " + str(results[i][0])
+		print "Sub-array: " +  str(results[i][1]) + "\n"
+			
+		assert(results[i][0] == 187)
+		assert(results[i][1] == [59, 26, -53, 58, 97])
+
+		
+		
+		i = i + 1
+
+
+
 array = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
-
-
 
 algorithms = [maxSubArray.maxSubArray1, \
 	maxSubArray.maxSubArray2, \
 	maxSubArray.maxSubArray3, \
 	maxSubArray.maxSubArray4]
 
-i = 1	
-for algorithm in algorithms:
-	(maxSum, subArray) = algorithm(array)
-	print "Algorithm ", i
-	print "Maximum Sum: " + str(maxSum)
-	print "Sub-array: " +  str(subArray) + "\n"
 		
-	assert(maxSum == 187)
-	assert(subArray == [59, 26, -53, 58, 97])
-
-	
-	
-	i = i + 1
+testArrayFunctions(array, algorithms)
