2129cbb00436bd8eb6bb387ddaf7027733f9e9fb
EvanSchallerer <schallee@onid.oregonstate.edu>
2015-07-17 20:17:19 -0700

    finished changedp

--- project2/changedp.py
+++ project2/changedp.py
@@ -1,36 +1,44 @@
-def changedp(value, denominations, memoizedVals):
-	coins = [0] * len(denominations)  #initialize to all zeros
-	
-	#base case
-	if value in denominations:
-		coins[denominations.index(value)] = coins[denominations.index(value)] + 1
-		memoizedVals[value] = coins
-		return coins	
-	
-	#check all combinations of value
-	for i in range(1, value):
-		coins1 = 0 
-		coins2 = 0
-		
-		if (memoizedVals[i] == False):	
-			coins1 = changedp(i, denominations, memoizedVals)
-		else: 
-			coins1 = memoizedVals[i]
-			
-		if (memoizedVals[value - i] == False):	
-			coins2 = changedp(value - i, denominations, memoizedVals)
-		else: 
-			coins2 = memoizedVals[value - i]
-		
-			
-		#store minimum number of coins (force first time)
-		if ((sum(coins1) + sum(coins2)) < sum(coins)) or (i == 1):
-			coins = [sum(x) for x in zip(coins1, coins2)]
-			memoizedVals[value] = coins
+import sys
+from dpImplementation import wrapDP, changedp
 
-	return coins
+if(len(sys.argv) < 2):
+	print "Did not specify filename.\nExiting"
+	exit(0)
+
+filename = sys.argv[1]
+filenameExt = filename[-4:]
+print "***" + filenameExt + "***"
+
+if(filenameExt not in ".txt"):
+	print "Invalid filetype.\nExiting."
+	exit(0)
+
+money = open(filename, 'r')
+
+#Redirect to a stupid name catted from in on command line
+filename = filename[:-4]
+sys.stdout = open("change" + filename + ".txt", "w+")
+
+amount = [0]
+
+while True:
+	values = money.readline()
+	if not values:
+		break
+	if values == [''] or values[0] == '#' or values[0] == '\n':
+		continue
+	amount = int(money.readline())
 	
+	values = values.rstrip('\n')
+	values = values.rstrip('\r')
+	values = values.rstrip(']')
+	values = values.lstrip('[')
+	values = values.split(',') 
+	values = map(int, values)
 	
-def wrapDP(value, denominations):
-	memoizedVals = [False] * (value+1)
-	return changedp(value, denominations, memoizedVals)
+	coinCount = [0] * len(values)
+	coinCount = wrapDP(amount, values)
+	print coinCount
+	print sum(coinCount)
+
+money.close()
